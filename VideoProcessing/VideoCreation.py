3c5d42311cc784ffd99ceacf7455551fda4000db
Darthinvader <psaradakisj@gmail.com>
2019-12-16 21:59:56 +0200

    Added audio to be optional

--- VideoProcessing/VideoCreation.py
+++ VideoProcessing/VideoCreation.py
@@ -1,8 +1,15 @@
 import moviepy.editor as mpy
 
 
-def runner(function, duration, filename, fps, audio):
+def runner(function, duration, filename, fps, audio=None):
     clip = mpy.VideoClip(function, duration=duration)
-    audio_clip = mpy.AudioFileClip(audio).set_duration(clip.duration)
-    clip.audio = audio_clip
+
+    # if the audio exists
+    if audio is not None:
+        # then open the audiofile and add it to the video
+        audio_clip = mpy.AudioFileClip(audio)
+        audio_clip.set_duration(clip.duration)
+        clip.audio = audio_clip
+
+    # write the videofile
     clip.write_videofile(filename, fps=fps)
