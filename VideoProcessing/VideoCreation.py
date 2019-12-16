aa2f0de8f06562f928e7d60ac590030fcd3124c1
Darthinvader <psaradakisj@gmail.com>
2019-12-16 23:15:44 +0200

    runner function audio can be a list containing more than filenames.
    VideoCreation.py runner function audio can be a list containing both filenames and moviepy.Audioclip(s)

--- VideoProcessing/VideoCreation.py
+++ VideoProcessing/VideoCreation.py
@@ -6,12 +6,22 @@ def runner(function, duration, filename, fps, audio=None):
 
     # if the audio exists
     if audio is not None:
-        wrong_audio_type = False
+        # If the audio is a list
         if type(audio) is list:
             audio_clip_list = list()
+            # Then we add all the audiofiles
             for audio_file in audio:
-                temp_audio_clip = mpy.AudioFileClip(audio)
+                if audio_file is str:
+                    temp_audio_clip = mpy.AudioFileClip(audio)
+                elif audio_file is mpy.AudioClip:
+                    temp_audio_clip = mpy.AudioFileClip(audio)
+                else:
+                    raise Exception(
+                        "Parameter audio that was passed as a list containing elements of incorrect variable type. "
+                        "Type must either be a filename string or a moviepy.Audioclip.")
+
                 audio_clip_list.append(temp_audio_clip)
+            # We then merge the audiofiles
             audio_clip = mpy.CompositeAudioClip(audio_clip_list)
 
         elif type(audio) is str:
@@ -20,7 +30,9 @@ def runner(function, duration, filename, fps, audio=None):
         elif type(audio) is mpy.AudioClip:
             audio_clip = audio
         else:
-            raise Exception("Parameter audio is incorrect type of variable.")
+            raise Exception(
+                "Parameter audio is incorrect type of variable. Type must either a filename string, "
+                "a moviepy. Audioclip or  list of moviepy.Audioclip or filename string.")
 
         audio_clip = audio_clip.set_duration(clip.duration)
         clip.audio = audio_clip
